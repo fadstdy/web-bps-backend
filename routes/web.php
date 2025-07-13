f007bbab842a11d33c110420e962755560735d32
fadzillakn <fadzillakusuma11@gmail.com>
2025-07-13 13:15:07 +0700

    Fix Procfile port configuration

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,8 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'message' => 'Laravel is running!',
+        'status' => 'success'
+    ]);
 });

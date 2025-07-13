af904ed633363291834900b20430c1edeceb1a25
fadzillakn <fadzillakusuma11@gmail.com>
2025-07-13 11:09:39 +0700

    ulang

--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,15 @@
 <?php
+
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'https://web-bps-framework.netlify.app',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
-    'supports_credentials' => true,
+    'supports_credentials'=>true,
 ];
